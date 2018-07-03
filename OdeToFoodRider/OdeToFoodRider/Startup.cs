032855d2ac63c7bdca103045f5da2b01cda12405
gorohoroh <gorohoroh@gmail.com>
2018-07-03 18:17:14 +0300

    Showing exception details + middleware to match the environment

--- OdeToFoodRider/OdeToFoodRider/Startup.cs
+++ OdeToFoodRider/OdeToFoodRider/Startup.cs
@@ -24,41 +24,17 @@ namespace OdeToFoodRider
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
-//            if (env.IsDevelopment())
-//            {    
-//                app.UseDeveloperExceptionPage();
-//            }
+            if (env.IsDevelopment())
+            {    
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.Use(next =>
-            {
-                // RDVS: Rider's completion doesn't expect an identifier after the async keyword, so it starts to suggest weird classes,
-                // and this is where completion on space hurts: I wanted to type "context" but completion triggered on Space and inserted the unwanted ContextBoundObject  
-                return async context =>
-                {
-                    // RDVS: again bad completion on unresolved symbol: wanted to use undeclared "logger" to add it as parameter later, but got Logger<> completed instead
-                    logger.LogInformation("Request incoming");
-                    if (context.Request.Path.StartsWithSegments("/mym"))
-                    {
-                        await context.Response.WriteAsync("Hit!!!");
-                        logger.LogInformation("Request handled");
-                    }
-                    else
-                    {
-                        await next(context);
-                        logger.LogInformation("Response outgoing");
-                    }
-                };
-            });
-
-            app.UseWelcomePage(new WelcomePageOptions()
-            {
-                Path = "/wp"
-            });
             
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
-                await context.Response.WriteAsync(greeting);
+                // RDVS: wanted to use string interpolation for the WriteAsync() argument but a context action is missing :(
+                await context.Response.WriteAsync($"{greeting}: {env.EnvironmentName}");
             });
         }
     }
